6a0acd8537f8eb3b8abf3bff142cb57031f3e590
dr1ll <bohrer1941@snafu.de>
2014-05-29 19:10:41 +0200

    Commandeer überarbeitet

--- hostname.py
+++ hostname.py
@@ -20,7 +20,7 @@ import os
 import socket
 import random
 import string
-# import commandeer
+import commandeer
 
 
 # vars
@@ -137,181 +137,72 @@ def setname(toset):
     os.system("hostname -b {0}".format(toset))
 
 
-os.system("clear")
-print"+++++++++++++++++++++++++ Linux-Hostnamen setzen ++++++++++++++++++++++++++++"
-print"v", version
-print
-print"Aktueller Hostname: ", HostName
-print"Hostname n. Reboot: ", StoredName
-print
-print("Für das Setzen eines Netzwerknamens stehen folgende Aktionen zur Verfügung:\n")
-print("1 - Temporären Zufalls-Hostnamen setzen lassen")
-print("2 - Temporären Hostnamen aus Namensliste setzen lassen")
-print("3 - Temporären Hostnamen aus Namensliste mit Modifikation setzen lassen")
-print("4 - Temporären Hostnamen selbst eingeben")
-print("5 - Permanenten Hostnamen in das System eingeben")
-print("6 - Aktuellen Hostnamen nur in Datei dauerhaft speichern")
-print("7 - Vormals gespeicherten Hostnamen reaktivieren (aus Datei resetten)")
-print
-print"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-print
-choice = str(raw_input("Geben Sie die Zahl der gewünschten Aktion (1-7) ein: "))
-
-NameList = makenamelist()
-
-if choice == "1":
-    nametoset = randomname()
-elif choice == "2":
-    nametoset = listname()
-elif choice == "3":
-    nametoset = combinename()
-elif choice == "4":
-    nametoset = ownname()
-elif choice == "5":
-    replacename()
-elif choice == "6":
-    filename()
-else:
-    fixname()
-
-if choice == "1" or choice == "2" or choice == "3" or choice == "4":
-    setname(nametoset)
-    weiter = False
-    while weiter == False:
-        weiter = True
-        answer = raw_input("\nAktuellen Hostnamen dauerhaft abspeichern (J/n)? > ")
-        if answer == "J" or answer == "j":
-            outputfile = open('/etc/hostname', 'w')
-            outputfile.write(nametoset)
-            outputfile.close()
-        elif answer == "n" or answer == "N":
-            pass
-        else:
-            weiter = False
-
-
-inputfile = open('/etc/hostname', 'r')
-SysName = inputfile.read()
-inputfile.close()
-HostName = socket.gethostname()
-
-print
-print"Hostname      vorher:", OldName
-print"Hostname     aktuell:", HostName
-print"Hostname nach Reboot:", SysName
-
-
-def nameload_command():
-    inputfile = open('/etc/hostname', 'r')
-    loadname = inputfile.read()
-    inputfile.close()
-    return loadname
-
-
-def namestore_command():
-    global hoststext
-    weiter = False
-    newname = ''
-    while weiter == False:
-        print"\n+++++++++++++++ 7: Permanenten Hostnamen nun manuell eingeben +++++++++++++++\n"
-        print"(Groß/Kleinschrift/Zahl - keine Leer/Sonderzeichen)"
-        weiter = True
-        newname = raw_input("\nEingabe >>> ")
-        i = 1
-        for c in newname:
-            if not c in Ascii and i == 1:
-                print"*** Fehler: ERSTER Buchstabe immer nur a-z oder A-Z! ***\n"
-                weiter = False
-                break
-            if not c in AllChars:
-                print"*** Fehler: Geben Sie nur a-z, A-Z, 0-9 ein! ***\n"
-                weiter = False
-                break
-            i += 1
-        inputfile = open('/etc/hostname', 'r')
-        hoststext = inputfile.read()
-        inputfile.close()
-        outputfile = open('/etc/hostname', 'w')
-        outputfile.write(newname)
-        outputfile.close()
-    return newname
-
-
-def setname_command(toset):
-    os.system("hostname -b {0}".format(toset))
-
-
-def savename_command():
-    weiter = False
-    while weiter == False:
-        weiter = True
-        answer = str(raw_input("\nHostname dauerhaft speichern (J/n)? > "))
-        if answer == ("J") or ("j"):
-            outputfile = open('/etc/hostname', 'w')
-            outputfile.write(nametoset)
-            outputfile.close()
-        elif answer == ("n") or ("N"):
-            break
-        else:
-            weiter = False
-
-def end():
-    hostname = socket.gethostname()
-    inputfile = open('/etc/hostname', 'r')
-    sysname = inputfile.read()
-    inputfile.close()
-    print
-    print"Hostname      vorher:  ", OldName
-    print"Hostname       aktiv:  ", hostname
-    print"Hostname nach Reboot:  ", sysname
-    print"\n+++++++++++++++++++++++++++++++++++ Ende ++++++++++++++++++++++++++++++++++++\n\n"
-
 def start_command():
     os.system("clear")
-    HostName = socket.gethostname()
-    print"\n+++++++++++++++++++++++++ Linux-Hostnamen setzen ++++++++++++++++++++++++++++"
-    print"v", version
-    print"Aktueller Hostname: ",HostName
+    print"+++++++++++++++++++++++++ Linux-Hostnamen setzen ++++++++++++++++++++++++++++"
+    print"Please execute as \"ROOT\"", "                                            v", version
+    print
+    print"Aktueller Hostname: ", HostName
+    print"Hostname n. Reboot: ", StoredName
     print
     print("Für das Setzen eines Netzwerknamens stehen folgende Aktionen zur Verfügung:\n")
-    print("1 - Temporären Hostnamen randomisiert setzen")
-    print("2 - Temporären Hostnamen aus Namensliste setzen")
-    print("3 - Temporären Hostnamen modifiziert aus Namensliste setzen")
+    print("1 - Temporären Zufalls-Hostnamen setzen lassen")
+    print("2 - Temporären Hostnamen aus Namensliste setzen lassen")
+    print("3 - Temporären Hostnamen aus Namensliste mit Modifikation setzen lassen")
     print("4 - Temporären Hostnamen selbst eingeben")
-    print("5 - Gespeicherten Hostnamen aus dem System aktivieren")
-    print("6 - Permanenten Hostnamen in das System dauerhaft schreiben")
+    print("5 - Permanenten Hostnamen in das System eingeben")
+    print("6 - Aktuellen Hostnamen nur in Datei dauerhaft speichern")
+    print("7 - Vormals gespeicherten Hostnamen reaktivieren (aus Datei resetten)")
     print
     print"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
     print
-    goon = False
-    choice = str(raw_input("Geben Sie Ihre Auswahl ein (1-6): >>>"))
-    while goon == False:
-        print
-        if choice == 1:
-            randomname(nametoset)
-            goon = True
-        if choice == 2:
-            listname(nametoset)
-            goon = True
-        if choice == 3:
-            combinename(nametoset)
-            goon = True
-        if choice == 4:
-            ownname(nametoset)
-            goon = True
-        if choice == 5:
-            nameload_command(nametoset)
-            goon = True
-        if choice == 6:
-            namestore_command(nametoset)
-            goon = True
-        else: choice = str(raw_input("Nochmal bitte (1-6): >>>"))
+    choice = str(raw_input("Geben Sie die Zahl der gewünschten Aktion (1-7) ein: "))
+
+    if choice == "1":
+        nametoset = randomname()
+    elif choice == "2":
+        nametoset = listname()
+    elif choice == "3":
+        nametoset = combinename()
+    elif choice == "4":
+        nametoset = ownname()
+    elif choice == "5":
+        replacename()
+    elif choice == "6":
+        filename()
+    else:
+        fixname()
+
+    if choice == "1" or choice == "2" or choice == "3" or choice == "4":
+        setname(nametoset)
+        weiter = False
+        while weiter == False:
+            weiter = True
+            answer = raw_input("\nAktuellen Hostnamen dauerhaft abspeichern (J/n)? > ")
+            if answer == "J" or answer == "j":
+                outputfile = open('/etc/hostname', 'w')
+                outputfile.write(nametoset)
+                outputfile.close()
+            elif answer == "n" or answer == "N":
+                pass
+            else:
+                weiter = False
+
+
+def end_command():
+    inputfile = open('/etc/hostname', 'r')
+    SysName = inputfile.read()
+    inputfile.close()
+    HostName = socket.gethostname()
+
+    print
+    print"Hostname      vorher:", OldName
+    print"Hostname     aktuell:", HostName
+    print"Hostname nach Reboot:", SysName
 
 
 if __name__ == '__main__':
     commandeer.cli()
-    makenamelist()
+    NameList = makenamelist()
     start_command()
-    setname_command()
-    savename_command()
-    end()
+    end_command()
